645f57a5ca6b077fb8027598237c2f49c230ac15
Sylvain <sylvain.garant@epitech.eu>
2015-06-13 10:37:37 +0200

    lold

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for main in /home/garant_s/AI
+**
+** Made by sylvain garant
+** Login   <garant_s@epitech.net>
+**
+** Started on  Sat Jun 13 10:33:33 2015 sylvain garant
+** Last update Sat Jun 13 10:34:00 2015 sylvain garant
+*/
+
+#include <unistd.h>
+
+int	main()
+{
+  write(1, "lold\n", 5);
+  return (0);
+}
